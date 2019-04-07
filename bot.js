e0cb999c1e5b52ae8e0fc0c7f05b8b2bbb135d4b
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-08 00:03:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ function clean(text) {
         return text;
 }
 
-const prefix = "Your Prefix";
-const token = "Token Your Bot";
+const prefix = "!";
+const token = "564473747420217346";
 
 client.on("ready", () => {
   console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
